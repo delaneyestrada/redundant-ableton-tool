4fffac78ccf8300c6d961a7cbb334ebb05462daf
Dillon Estrada <dillon.estrada55@gmail.com>
2020-07-15 15:53:01 -0500

    Add directory transfer script functionality

---
+++ transfer.py
@@ -0,0 +1,38 @@
+import paramiko
+import os
+
+class MySFTPClient(paramiko.SFTPClient):
+    def put_dir(self, source, target):
+        ''' Uploads the contents of the source directory to the target path. The
+            target directory needs to exists. All subdirectories in source are 
+            created under target.
+        '''
+        for item in os.listdir(source):
+            if os.path.isfile(os.path.join(source, item)):
+                self.put(os.path.join(source, item), '%s/%s' % (target, item))
+            else:
+                self.mkdir('%s/%s' % (target, item), ignore_existing=True)
+                self.put_dir(os.path.join(source, item), '%s/%s' % (target, item))
+
+    def mkdir(self, path, mode=511, ignore_existing=False):
+        ''' Augments mkdir by adding an option to not fail if the folder exists  '''
+        try:
+            super(MySFTPClient, self).mkdir(path, mode)
+        except IOError:
+            if ignore_existing:
+                pass
+            else:
+                raise
+
+hostname = "DE-LAPTOP"
+username = 'dillon.estrada55@gmail.com'
+source_path = 'D:/Dillon Estrada/Documents/Ableton'
+target_path = 'C:/users/dillo/test/test'
+password=input("Remote computer password: ")
+
+transport = paramiko.Transport((hostname, 22))
+transport.connect(username=username, password=password)
+sftp = MySFTPClient.from_transport(transport)
+sftp.mkdir(target_path, ignore_existing=True)
+sftp.put_dir(source_path, target_path)
+sftp.close()
